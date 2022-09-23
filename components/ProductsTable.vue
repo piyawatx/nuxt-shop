fb7ff1b0b6d6eeeec3feb4ff83e81c86fafc2622
piyawat <piyawat>
2022-09-23 23:25:01 +0700

    จัด table หน้า Products ใหม่

--- components/ProductsTable.vue
+++ components/ProductsTable.vue
@@ -1,31 +1,76 @@
 <template>
-  <table class="table table-hover table-bordered">
-    <thead>
-      <tr>
-        <th scope="col" class="text-center">#</th>
-        <th scope="col">Title</th>
-        <th scope="col">Price</th>
-        <th scope="col">Image</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr
-        v-for="(item, index) in this.$store.getters.getProducts"
-        :key="item.id"
-      >
-        <th class="text-center">{{ index + 1 }}</th>
-        <td>{{ item.title }}</td>
-        <td class="text-end">{{ item.price.toLocaleString() }}</td>
-        <td><img :src="item.imageUrl" alt="" /></td>
-      </tr>
-    </tbody>
-  </table>
+  <div>
+    <div class="card">
+      <div class="card-body p-0">
+        <div class="table-responsive">
+          <table class="table table-hover table-borderless table-striped m-0 align-middle">
+            <thead>
+              <tr>
+                <th class="text-center">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    @click="checkAll()"
+                    v-model="isCheckAll"
+                  />
+                </th>
+                <th scope="col">Image</th>
+                <th scope="col">Title</th>
+                <th scope="col">Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="item in this.$store.getters.getProducts"
+                :key="item.id"
+              >
+                <td class="text-center">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    :value="item.id"
+                    v-model="checkProducts"
+                    @click="select"
+                  />
+                </td>
+
+                <td><img :src="item.imageUrl" alt="" /></td>
+                <td>{{ item.title }}</td>
+                <td>{{ item.price.toLocaleString() }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   created() {
     this.$store.dispatch('fetchProducts')
+  },
+  data() {
+    return {
+      isCheckAll: false,
+      checkProducts: [],
+    }
+  },
+  methods: {
+    checkAll() {
+      this.isCheckAll = !this.isCheckAll
+      this.checkProducts = []
+      let products = this.$store.getters.getProducts
+      if (this.isCheckAll) {
+        products.forEach((product) => {
+          this.checkProducts.push(product.id)
+        })
+      }
+    },
+    select() {
+      this.isCheckAll = false
+    },
   },
 }
 </script>
